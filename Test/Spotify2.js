a061e4f301ef7735647baa2a534a3244a8970771
Pablis369 <pvm00@iesemilidarder.com>
2019-11-22 13:15:37 +0100

    Prueba

--- Test/Spotify2.js
+++ Test/Spotify2.js
@@ -1,12 +1,21 @@
 {
-    Generos:[{name: "POP",
-        Canciones:[
-            {
-
-            }
+    "Artistas"[
+    {"name":"Ne-yo","id":"ne-yo"}
+    ,{"name":"Martin Garrix","id":"martin_garrix"}
+    ,{"name":"AnuelAA","id":"anuelaa"}
+]
+    "Generos"[
+        {"name":"POP","id":"pop"}
+        ,{"name":"ROCK","id":"rock"}
+        ,{"name":"DANCE","id":"dance"}
+        ,{"name":"REGGAETON","id":"reggaeton"}
         ]
-
-    }
-
-        ]
+     "Canciones"[
+         {"id":"so_sick_ne-yo","idArtista":"ne-yo",
+             "IdGenero":"pop","name":"SoSick.mp3"}
+          ,{"id":"animals_martin_garrix","idArtista":"martin_garrix",
+             "IdGenero":"dance","name":"Animals.mp3"}
+             ,{"id":"por_ley_anuelaa","idArtista":"anuelaa",
+                "IdGenero":"reggaeton","name":"PorLey.mp3"}
+         ]
 }
