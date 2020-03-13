1dd0f85cde0faca45a00eceff2c410c51f733857
Pablis963 <pvm00@iesemilidarder.com>
2020-03-13 17:03:29 +0100

    13/03/2020

---
+++ 1-react-baseapp/src/components/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Menu() {
+    return(
+        <ul>
+            <li>Opc1</li>
+            <li>Opc2</li>
+            <li>Opc3</li>
+        </ul>
+    );
+}
+
+export default Menu;
